8c96c4dc6a8f62a0c892d13ae6d62311065065cb
nekkuzuria <amandafarliana10@gmail.com>
2023-09-07 15:02:13 +0700

    initial commit

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,3 +24,6 @@ Route::get('/about', function () {
         "email" => "lala@gmail.com"
     ]);
 });
+
+
+Route::get('/boom', [PostController::class, 'boomesport']);
